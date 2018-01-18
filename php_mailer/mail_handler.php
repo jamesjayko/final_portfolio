a86b5a88d3d5432a152ee7d238cc26811e69cad9
jay <jamesjayko@gmail.com>
2018-01-18 12:36:36 -0800

    initial commit

---
+++ php_mailer/mail_handler.php
@@ -0,0 +1,88 @@
+<?php
+require_once('email_config.php');
+require('phpmailer/PHPMailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->SMTPDebug = 3;           // 3 is Enable verbose debug output. Change to 0 to disable debugging output.
+
+$message = [
+    'email' => $_POST['email'],
+    'subject' => $_POST['subject'],
+    'body' => $_POST['message']
+];
+
+$output =[
+    'success' => null,
+    'message' => []
+];
+
+//Sanitize name field
+$message['name'] = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+if(empty($message['name'])){
+    $output['success'] = false;
+    $output['messages'][] = 'missing name key';
+}
+//Validate email field
+$message['email'] = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+if(empty($message['email'])){
+    $output['success'] = false;
+    $output['messages'][] = 'invalid email key';
+}
+//Sanitize message
+$message['message'] = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+if(empty($message['message'])){
+    $output['success'] = false;
+    $output['messages'][] = 'missing message key';
+}
+
+$mail->isSMTP();                // Set mailer to use SMTP.
+$mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers.
+$mail->SMTPAuth = true;         // Enable SMTP authentication
+
+$mail->Username = EMAIL_USER;   // SMTP username
+$mail->Password = EMAIL_PASS;   // SMTP password
+$mail->SMTPSecure = 'tls';      // Enable TLS encryption, `ssl` also accepted, but TLS is a newer more-secure encryption
+$mail->Port = 587;              // TCP port to connect to
+$options = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
+
+$mail->smtpConnect($options);
+$mail->From = $message['email'];  // sender's email address (shows in "From" field)
+$mail->FromName = $message['name'];   // sender's name (shows in "From" field)
+$mail->addAddress(EMAIL_USER);  // Add a recipient
+//$mail->addAddress('ellen@example.com');                        // Name is optional
+$mail->addReplyTo($message['email'], $message['name']);                          // Add a reply-to address
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = $_POST['subject'];
+$mail->Body    = $_POST['body'];
+$mail->AltBody = htmlentities($_POST['body']);
+
+if ($output['success'] !== null) {
+    http_response_code(400);
+    echo json_encode($output);
+    exit();
+}
+
+$message['message'] = nl2br($message['message']);
+
+if(!$mail->send()) {
+    $output['success'] = false;
+    $output['message'][] = $mail->ErrorInfo;
+    // echo 'Message could not be sent.';
+    // echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    $output['success'] = true;
+    // echo 'Message has been sent';
+}
+print(json_encode($output));
+?>
